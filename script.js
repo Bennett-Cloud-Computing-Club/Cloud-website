c0e381c08e69c845077cc28551e0e9a8c6f29bd1
Vaibhav Kaushik <vaibhavsingh5373@gmail.com>
2023-09-01 11:35:43 +0530

    blur-effect of cursor

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-var crsr = document.querySelector("#cursor")
+var crsr = document.querySelector("#cursor");
+var blur = document.querySelector("#cursor-blur");
 
-document.addEventListener("mousemove", function(e) {
-    crsr.style.left = e.x-10+"px"
-    crsr.style.top =e.y-10+"px"
-
-
+document.addEventListener("mousemove", function (dets) {
+  crsr.style.left = dets.x + "px";
+  crsr.style.top = dets.y + "px";
+  blur.style.left = dets.x - 250 + "px";
+  blur.style.top = dets.y - 250 + "px";
 });
